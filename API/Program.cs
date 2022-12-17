3f157c6ddf492c6358b9c92c071bb9d9857373d7
BogdanYarotsky <byarotsky@gmail.com>
2022-12-17 20:26:57 +0100

    Added client

--- API/Program.cs
+++ API/Program.cs
@@ -8,6 +8,8 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite(connectionString);
 });
 builder.Services.AddControllers();
+builder.Services.AddCors();
 var app = builder.Build();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 app.Run();
